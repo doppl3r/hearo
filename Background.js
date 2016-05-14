dcb489a44592dddbe36bcad3677a40802470f308
Jacob DeBenedetto <deben3@gmail.com>
2016-05-13 18:28:05 -0700

    adjust manifest for background

--- Background.js
+++ Background.js
@@ -1,24 +1,11 @@
 (function (window) {
 
 	//constructor
-	function Background() {
+	function Background(asset) {
 		this.Container_constructor();
-        this.addChild(this.sprite);
-	}
-
-	//instance of class
-	var container = createjs.extend(Background, createjs.Container);
-
-    //Public Properties
-    this.manifest = [{src: "background-image1.png", id: "bg1"},{src: "background-image2.png", id: "bg2"}];
-    container.loader = new createjs.LoadQueue(false);
-    container.loader.addEventListener("complete", handleComplete);
-    container.loader.loadManifest(this.manifest, true, "img/");
-
-    function handleComplete() {
-        container.spriteSheet = new createjs.SpriteSheet({
+        this.spriteSheet = new createjs.SpriteSheet({
             framerate: 1,
-            images: [container.loader.getResult("bg2")],
+            images: [asset],
             frames: {"width": 1280, "height": 720, "count": 1},
             // define two animations, run (loops, 1.5x speed) and jump (returns to run):
             animations: {
@@ -26,10 +13,15 @@
                 frame2: [1]
             }
         });
-        container.sprite = new createjs.Sprite(container.spriteSheet,"frame1");
-        container.sprite.x = -(container.sprite.getBounds().width/2);
-        container.sprite.y = -(container.sprite.getBounds().height/2);
-    }
+        this.sprite = new createjs.Sprite(this.spriteSheet,"frame1");
+        this.sprite.x = -(this.sprite.getBounds().width/2);
+        this.sprite.y = -(this.sprite.getBounds().height/2);
+        this.addChild(this.sprite);
+	}
+
+	//instance of class
+	var container = createjs.extend(Background, createjs.Container);
+
     //update
 	container.tick = function (event) { }
 
